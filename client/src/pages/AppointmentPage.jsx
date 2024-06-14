b7c77b9a2d43a2d98b429c9610d3b2bb816e19b3
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-06-14 13:04:28 -0400

    Appointment component created, Appointments page updated to fetch appointments and display

---
+++ client/src/pages/AppointmentPage.jsx
@@ -0,0 +1,43 @@
+import React,{useState, useEffect} from react
+import {Link} from "react-router-dom"
+import Appointment from "../components/Appointment"
+
+function AppointmentPage(){
+
+    const [appointments, setAppointments] = useState([])
+
+    useEffect(()=>{
+        fetch("/api/appointments")
+      .then(r =>{
+        if (r.ok){
+          r.json().then(appointments=>{
+            setAppointments(appointments)
+        });
+        };
+      });
+    },[]);
+    
+    const displayAppointments = appointments.map(appointment=>{
+        return <Appointment appointment={appointment}/>
+    })
+
+    if(!loggedIn){
+        return(
+            <div>
+                <p>You must be logged in view appointments and patients</p>
+                <Link to = "/login">
+                    <button className="bg-transparent hover:bg-blue-900 text-green-700 font-semibold hover:text-white py-2 px-4 border border-blue-300 hover:border-transparent rounded">Login</button>
+                </Link>
+            </div>
+        )
+    }
+
+    return(
+        <div>
+            <h2>Here are your current appointments. Please review prior to seeing your patients</h2>
+            {displayAppointments}
+        </div>
+    )
+}
+
+export default AppointmentPage
