a3b8685f06bc463bee72fa9a13afeb7d07fa143f
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-06-13 23:07:14 -0400

    SerializerMixin, hybrid_property, and validates imported ; bcrypt and db imported from config.py

---
+++ server/models.py
@@ -0,0 +1,7 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import validates
+from config import bcrypt
+
+from config import db
+
