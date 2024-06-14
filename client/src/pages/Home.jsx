d73911c2c11c7338cfbf1486c8fff6d15ab3276e
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-06-14 12:30:35 -0400

    Home page updated

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
+function Home(){
+
+    if(!loggedIn){
+        return(
+            <div>
+                <p>Please Log in to Use our Services</p>
+                <Link to = "/login">
+                    <button className="bg-transparent hover:bg-blue-900 text-green-700 font-semibold hover:text-white py-2 px-4 border border-blue-300 hover:border-transparent rounded">Login</button>
+                </Link>
+            </div>
+        )
+    }
+
+    return(
+        <div>
+            <p>
+                Welcome to One Medical Central.
+                We hope that you are having a good day so far. 
+                Best wishes in your care to your patients.
+                Did you know that an owl thats a doctor would be called Dr. Who? - Just some Medical Comedy
+                Feel free to view your orders, appointments, or patients using one of the tabs above 
+            </p>
+            <p>
+                Please hit log out before leaving your station to protect your patients' information. Thank you!
+            </p>
+        </div>
+    )
+}
+
+export default Home
