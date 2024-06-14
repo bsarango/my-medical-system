61ffcb1f68346c215497b7252327d91ff06d83b6
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-06-14 12:46:48 -0400

    Signup paged updated

---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SignupForm from "../components/SignupForm"
+
+function Signup(){
+
+    return(
+        <div className="bg-yellow-100">
+            <h2 className="font-bold text-lg">Enter the following fields to create an account with us!</h2>
+            <br></br>
+            <SignupForm/>
+        </div>
+    )
+}
+
+export default Signup
