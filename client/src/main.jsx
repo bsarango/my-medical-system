13a186740dc34b67c76d9b5746ed342bd9d5f196
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-06-14 12:14:17 -0400

    react-router-dom@6 installed, router for pages created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,46 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import Home from './pages/Home.jsx'
+import Signup from './pages/Signup.jsx'
+import Login from './pages/Login.jsx'
+import AppointmentPage from './pages/AppointmentPage.jsx'
+import Orders from './pages/Orders.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/signup",
+        element:<Signup/>
+      },
+      {
+        path:"/login",
+        element:<Login/>
+      },
+      {
+        path:"/appointments",
+        element:<AppointmentPage/>
+      },
+      {
+        path:"/orders",
+        element:<Orders/>
+      }
+    ]
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <RouterProvider router = {router}/>
 )
