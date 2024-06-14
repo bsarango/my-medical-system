b45050135bd1981c1a215ed9306ff462c1352c0c
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-06-14 12:33:35 -0400

    LoggedIn page updated

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import React,{useEffect, setState} from 'react'
+import LoginForm from '../components/LoginForm'
+
+function Login(){
+
+    if(loggedIn){
+        return(
+            <p>Already Logged In. Please return Home or select an option from the tabs to continue</p>
+        )
+    }
+
+    return(
+        <div className = "text-center bg-yellow-100">
+        <h3 className="font-sans font-semibold">Enter your credentials below and press submit to Sign In</h3>
+        <br></br>
+        <LoginForm/>
+    </div>
+    )
+}
+
+export default Login
