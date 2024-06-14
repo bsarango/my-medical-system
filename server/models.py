53f99e981535332dfafe3911d73c9352702b8b3b
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-06-13 23:10:30 -0400

    Physician , Patient, Appointment, Order models established in models.py

--- server/models.py
+++ server/models.py
@@ -5,3 +5,14 @@ from config import bcrypt
 
 from config import db
 
+class Physician(db.Model, SerializerMixin):
+    __tablename__ = "physicians"
+
+class Patient(db.Model, SerializerMixin):
+    __tablename__="patients"
+
+class Appointment(db.Model, SerializerMixing):
+    __tablename__="appointments"
+
+class Orders(db.Model, SerializerMixin):
+    __tablename__="orders"
