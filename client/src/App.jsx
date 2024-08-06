e14c93ff422c12ab1c561126f103f2e0c7e61eeb
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-08-06 09:21:09 -0400

    tailwindcss removed from index and App.css; NavBar renamed to NavigationBar; component name udpated in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import NavBar from './components/Navbar'
+import NavigationBar from './components/Navigationbar'
 import {Outlet} from "react-router-dom"
 import './App.css'
 
@@ -21,7 +21,7 @@ function App() {
     <div className="bg-blue-200">
       <header className = ""> 
         <h1 className="center-text">One Medical Central</h1>
-        <NavBar/>
+        <NavigationBar/>
         <button className="bg-transparent hover:bg-blue-900 text-green-700 font-semibold hover:text-white py-2 px-4 border border-blue-300 hover:border-transparent rounded" onClick={e=>{setLoggedIn(false)}}>LogOut</button>
       </header>
       <Outlet context={{loggedIn:[loggedIn,setLoggedIn]}}/>
---
+++ client/src/components/Navigationbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+
+function NavigationBar(){
+    return(
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <NavLink  to="/"
+                    className=""
+                >
+                    Home
+                </NavLink>
+                <NavLink  to="/signup"
+                    className ="" 
+                >
+                    SignUp
+                </NavLink>
+                <NavLink  to="/login"
+                    className = ""
+                >
+                    Login
+                </NavLink>
+                <NavLink  to="/appointments"
+                    className = ""
+                >
+                    My Appointments
+                </NavLink>
+                <NavLink  to="/orders"
+                    className =""
+                >
+                    My Orders
+                </NavLink>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavigationBar
