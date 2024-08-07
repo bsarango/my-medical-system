c46e7c67c90765cc8b05c49d8490ef725df68fcc
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-08-07 11:43:00 -0400

    h1 tag removed in App.jsx, SignUpForm component updated, App.css - removed unused css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,11 +19,11 @@ function App() {
   },[]);
 
   return (
-    <div className="bg-blue-200">
+    <div>
       <header className = ""> 
-        <h1 className="center-text">One Medical Central</h1>
+        {/* <h1 className="center-text">One Medical Central</h1> */}
         <NavigationBar/>
-        <button className="bg-transparent hover:bg-blue-900 text-green-700 font-semibold hover:text-white py-2 px-4 border border-blue-300 hover:border-transparent rounded" onClick={e=>{setLoggedIn(false)}}>LogOut</button>
+        <button className="" onClick={e=>{setLoggedIn(false)}}>LogOut</button>
       </header>
       <Outlet context={{loggedIn:[loggedIn,setLoggedIn]}}/>
     </div>
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -41,103 +41,122 @@ function SignupForm(){
 
     return(
         <div>
-            <form className = "text-center" onSubmit={handleSubmit}>
-                <label htmlFor="username">Username</label>
-                <input
-                    className="border-solid border-2 border-green-600 py-1 space-y-1"
-                    type = "text"
-                    name = "username"
-                    value = {username}
-                    onChange = {e=>setUsername(e.target.value)}
-                />
-                <br></br>
-                <label htmlFor="password">Password</label>
-                <input
-                    className="border-solid border-2 border-green-600 py-1 space-y-1"
-                    type = "text"
-                    name = "password"
-                    value = {password}
-                    onChange = {e=>setPassword(e.target.value)}
-                />
-                <br></br>
-                <label htmlFor='firstName'>First Name</label>
-                <input
-                    className="border-solid border-2 border-green-600 py-1 space-y-1"
-                    type = "text"
-                    name = "firstName"
-                    value = {firstName}
-                    onChange = {e=>setFirstName(e.target.value)}
-                />
-                <br></br>
-                <label htmlFor='lastName'>Last Name</label>
-                <input
-                    className="border-solid border-2 border-green-600 py-1 space-y-1"
-                    type = "text"
-                    name = "lastName"
-                    value = {lastName}
-                    onChange = {e=>setLastName(e.target.value)}
-                />
-                <br></br>
-                <label htmlFor='officeAddress'>Office Address</label>
-                <input
-                    className="border-solid border-2 border-green-600 py-1 space-y-1"
-                    type = "text"
-                    name = "officeAddress"
-                    value = {officeAddress}
-                    onChange = {e=>setOfficeAddress(e.target.value)}
-                />
-                <br></br>
-                <label htmlFor='officeNumber'>Office Phone Number</label>
-                <input
-                    className="border-solid border-2 border-green-600 py-1 space-y-1"
-                    type = "text"
-                    name = "officeNumber"
-                    value = {officeNumber}
-                    onChange = {e=>setOfficeNumber(e.target.value)}
-                />
-                <br></br>
-                <select onChange={e=>{setSpecialty(e.target.value)}}>
-                    <option
-                        value = "cardiology"
-                    >
-                        Cardiology
-                    </option>
-                    <option
-                        value = "pediatrics"
-                    >
+            <form className = "" onSubmit={handleSubmit}>
+                <div className="p-2">
+                    <label htmlFor="username">Username</label>
+                    <input
+                        className="border-solid border-2 border-green-600 py-1 space-y-1"
+                        type = "text"
+                        name = "username"
+                        value = {username}
+                        onChange = {e=>setUsername(e.target.value)}
+                    />
+                </div>
+                <div className="p-2">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        className="border-solid border-2 border-green-600 py-1 space-y-1"
+                        type = "text"
+                        name = "password"
+                        value = {password}
+                        onChange = {e=>setPassword(e.target.value)}
+                    />
+                </div>
+                
+                <div className="p-2">
+                    <label htmlFor='firstName'>First Name</label>
+                    <input
+                        className="border-solid border-2 border-green-600 py-1 space-y-1"
+                        type = "text"
+                        name = "firstName"
+                        value = {firstName}
+                        onChange = {e=>setFirstName(e.target.value)}
+                    />
+                </div>
+                
+                <div className='p-2'>
+                    <label htmlFor='lastName'>Last Name</label>
+                    <input
+                        className="border-solid border-2 border-green-600 py-1 space-y-1"
+                        type = "text"
+                        name = "lastName"
+                        value = {lastName}
+                        onChange = {e=>setLastName(e.target.value)}
+                    />
+                </div>
+                
+                <div className='p-2'>
+                    <label htmlFor='officeAddress'>Office Address</label>
+                    <input
+                        className="border-solid border-2 border-green-600 py-1 space-y-1"
+                        type = "text"
+                        name = "officeAddress"
+                        value = {officeAddress}
+                        onChange = {e=>setOfficeAddress(e.target.value)}
+                    />
+                </div>
+                
+                <div>
+                    <label htmlFor='officeNumber'>Office Phone Number</label>
+                    <input
+                        className="border-solid border-2 border-green-600 py-1 space-y-1"
+                        type = "text"
+                        name = "officeNumber"
+                        value = {officeNumber}
+                        onChange = {e=>setOfficeNumber(e.target.value)}
+                    />
+                </div>
+                
+                <div className='p-2'>
+                    <select onChange={e=>{setSpecialty(e.target.value)}}>
+                        <option
+                            value = "cardiology"
+                        >
+                            Cardiology
+                        </option>
+                        
+                        <option
+                            value = "pediatrics"
+                        >
                         Pediatrics
-                    </option>
-                    <option
-                        value="primary care"
-                    >
-                        Primary Care
-                    </option>
-                    <option
-                        value="nephrology"
-                    >
-                        Nephrology
-                    </option>
-                    <option
-                        value="obstetrics and gynecology"
-                    >
-                        OBGYN
-                    </option>
-                    <option
-                        value="pulmonary"
-                    >
+                        </option>
+                    
+                        <option
+                            value="primary care"
+                        >
+                            Primary Care
+                        </option>
+                        <option
+                            value="nephrology"
+                        >
+                            Nephrology
+                        </option>
+                        
+                        <option
+                            value="obstetrics and gynecology"
+                        >
+                            OBGYN
+                        </option>
+                        
+                        <option
+                            value="pulmonary"
+                        >
                         Pulmonary
-                    </option>
-                    <option
-                        value="endocrinology"
-                    >
-                        Endocrinology
-                    </option>
-                    <option
-                        value='dermatology'
-                    >
-                        Dermatology
-                    </option>
-                </select>
+                        </option>
+                        
+                        <option
+                            value="endocrinology"
+                        >
+                            Endocrinology
+                        </option>
+                        <option
+                            value='dermatology'
+                        >
+                            Dermatology
+                        </option>
+                    </select>
+                </div>
+                
                 <button type="submit" className="bg-green-200 hover:bg-emerald-900 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-300 hover:border-transparent rounded">SignUp</button>
             </form>
         </div>
